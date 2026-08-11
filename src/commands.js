0e2a408479f2b267d87ca9bc9e15152979041c3f
toujouphilic <129611238+toujouphilic@users.noreply.github.com>
2026-08-11 19:20:07 -0400

    Update commands.js

--- src/commands.js
+++ src/commands.js
@@ -8,49 +8,72 @@ export const notifCommand = new SlashCommandBuilder()
   .setName("notif")
   .setDescription("Manage Twitch notifications")
   .setDMPermission(false)
-  .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
+  .setDefaultMemberPermissions(
+    PermissionFlagsBits.ManageGuild
+  )
 
   .addSubcommand((subcommand) =>
     subcommand
       .setName("setup")
-      .setDescription("Set the channel for Twitch notifications")
+      .setDescription(
+        "Set the channel or thread for Twitch notifications"
+      )
       .addChannelOption((option) =>
         option
           .setName("channel")
-          .setDescription("Channel where Twitch notifications will be sent")
-          .addChannelTypes(ChannelType.GuildText)
-          .setRequired(true),
-      ),
+          .setDescription(
+            "Channel or thread where Twitch notifications will be sent"
+          )
+          .addChannelTypes(
+            ChannelType.GuildText,
+            ChannelType.PublicThread,
+            ChannelType.PrivateThread,
+            ChannelType.AnnouncementThread
+          )
+          .setRequired(true)
+      )
   )
 
   .addSubcommand((subcommand) =>
     subcommand
       .setName("add")
-      .setDescription("Add a Twitch streamer to notifications")
+      .setDescription(
+        "Add a Twitch streamer to notifications"
+      )
       .addStringOption((option) =>
         option
           .setName("twitch")
-          .setDescription("Twitch username or channel URL")
-          .setRequired(true),
-      ),
+          .setDescription(
+            "Twitch username or channel URL"
+          )
+          .setRequired(true)
+      )
   )
 
   .addSubcommand((subcommand) =>
     subcommand
       .setName("remove")
-      .setDescription("Remove a Twitch streamer from notifications")
+      .setDescription(
+        "Remove a Twitch streamer from notifications"
+      )
       .addStringOption((option) =>
         option
           .setName("twitch")
-          .setDescription("Twitch username or channel URL")
-          .setRequired(true),
-      ),
+          .setDescription(
+            "Twitch username or channel URL"
+          )
+          .setRequired(true)
+      )
   )
 
   .addSubcommand((subcommand) =>
     subcommand
       .setName("list")
-      .setDescription("List all tracked Twitch streamers"),
+      .setDescription(
+        "List all tracked Twitch streamers"
+      )
   );
 
-export const commands = [notifCommand.toJSON()];
+export const commands = [
+  notifCommand.toJSON(),
+];
